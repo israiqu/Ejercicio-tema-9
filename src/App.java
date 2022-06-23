c7cd7abe9e3f43b42d8720999cf5d227e0887ddc
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-22 20:37:28 -0500

    Final commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Hello, World!");
+
+        Cliente cliente = new Cliente();
+
+        cliente.edad = 25;
+        cliente.nombre = "Ana";
+        cliente.telefono = "30-0565-0009";
+        cliente.credito = 5000.99;
+
+        System.out.println("Hola " + cliente.nombre + ", Veo que tienes " + cliente.edad
+                + " años y que tu número de teléfono es " + cliente.telefono + ". Te informo que tu crédito es $" + cliente.credito);
     }
 }
 
+class Persona {
+    int edad;
+    String nombre;
+    String telefono;
+} 
+
+class Cliente extends Persona {
+    double credito;
+}
+
+class Trabajador extends Persona {
+    double salario;
+}
